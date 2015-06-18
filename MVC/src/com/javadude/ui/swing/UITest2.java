205ee4b18ca6b1dd85dbaa4d90b1bd0808a794a6
Scott Stanchfield <scott@javadude.com>
2015-06-18 03:44:08 -0400

    Mutiple Views, Mutiple Models

--- MVC/src/com/javadude/ui/swing/UITest2.java
+++ MVC/src/com/javadude/ui/swing/UITest2.java
@@ -11,13 +11,20 @@ public class UITest2 {
 
 	@SuppressWarnings("serial")
 	public static void main(String[] args) {
-		TodoList todoList = new TodoList();
+		TodoList todoList1 = new TodoList();
+		TodoList todoList2 = new TodoList();
 		TodoListView view1 = new TodoListView();
-		view1.setModel(todoList);
+		TodoListView view2 = new TodoListView();
+		TodoListView view3 = new TodoListView();
+		view1.setModel(todoList1);
+		view2.setModel(todoList1);
+		view3.setModel(todoList2);
 		
 		new JFrame() {{
 			setLayout(new GridLayout(0, 1, 5, 5));
 			add(view1);
+			add(view2);
+			add(view3);
 			setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 			pack();
 			setVisible(true);
